7ade20c177242eec4e012e9592e25703f693fd9c
Danon Dev <danydanondev@gmail.com>
2023-09-19 00:42:32 +0200

    Had set up a tailwind config file with colors and main font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        colors: {
+            'main -white': '#FFFFFF',
+            'main-dark-green': '#114D46',
+            'main-green': '#62BB65',
+            'main-black': '#000000',
+            'main-shadow': '#F2F2F2',
+        },
+        extend: {
+            fontFamily: {
+                mulish: ['Mulish', 'sans-serif'],
+            },
+        },
     },
     plugins: [],
 };
